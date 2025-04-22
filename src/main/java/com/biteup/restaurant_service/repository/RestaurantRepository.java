e72124ce2f7b1cc25a11330dbd6cd764a0e7631f
AkilaManu <akilamanujith@gmail.com>
2025-04-22 23:03:32 +0530

    create restaurent and connect with cart

--- src/main/java/com/biteup/restaurant_service/repository/RestaurantRepository.java
+++ src/main/java/com/biteup/restaurant_service/repository/RestaurantRepository.java
@@ -1,9 +1,11 @@
 package com.biteup.restaurant_service.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.biteup.restaurant_service.model.Restaurant;
 
 public interface RestaurantRepository extends MongoRepository<Restaurant, String>{
-
+    Optional<Restaurant> findByEmail(String email);
 }
