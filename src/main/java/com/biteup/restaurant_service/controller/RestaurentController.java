6d02a359bc0af722e33d3ea2087cde6be4893d3b
gitbuddhila <buddhilalakshan12@gmail.com>
2025-04-26 18:08:17 +0530

    update and delete

--- src/main/java/com/biteup/restaurant_service/controller/RestaurentController.java
+++ src/main/java/com/biteup/restaurant_service/controller/RestaurentController.java
@@ -8,9 +8,11 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -48,4 +50,25 @@ public class RestaurentController {
     );
     return ResponseEntity.ok(restaurant);
   }
+  @PutMapping("/{id}")
+    public ResponseEntity<RestaurentResponseDTO> updateRestaurant(
+            @PathVariable String id,
+            @RequestBody RestaurentRequestDTO requestDTO) {
+        RestaurentResponseDTO res = restaurantService.updateRestaurant(id, requestDTO);
+        if (res.getError() != null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(res);
+        } else {
+            return ResponseEntity.ok(res);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteRestaurant(@PathVariable String id) {
+        boolean deleted = restaurantService.deleteRestaurant(id);
+        if (deleted) {
+            return ResponseEntity.ok("Restaurant deleted successfully.");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Restaurant not found.");
+        }
+    }
 }
