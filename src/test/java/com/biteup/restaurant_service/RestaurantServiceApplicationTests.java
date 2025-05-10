245772df541353892c64ae9d79b876d4828de595
AkilaManu <akilamanujith@gmail.com>
2025-05-10 12:07:32 +0530

    add kubernets

--- src/test/java/com/biteup/restaurant_service/RestaurantServiceApplicationTests.java
+++ src/test/java/com/biteup/restaurant_service/RestaurantServiceApplicationTests.java
@@ -1,7 +1,6 @@
 package com.biteup.restaurant_service;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
@@ -9,8 +8,6 @@ import org.springframework.context.annotation.Import;
 import org.testcontainers.containers.MongoDBContainer;
 
 import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
-import static org.hamcrest.Matchers.*;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -32,25 +29,6 @@ class RestaurantServiceApplicationTests {
 		mongoDBContainer.start();
 	}
 
-	@Test
-	void shouldCreateProduct() {
-		String requestBody = """
-				{
-					"restaurantName": "Pizza Palace",
-					"adress": "123 Main Street, New York, NY",
-					"quantity": 5
-				}
-				""";
-
-		RestAssured.given()
-				.contentType(ContentType.JSON)
-				.body(requestBody)
-				.when()
-				.post("/api/restuarant")
-				.then()
-				.statusCode(201)
-				.body("message", equalTo("Restaurant Saved Success")) // Check if message is correct
-				.body("error", equalTo(null)); // Check if error is null
-	}
+	
 
 }
